90bdd175f6e81f13a1bf59481b1b57e1e83491ee
shivani verma <vermashivi493@gmail.com>
2024-09-05 18:26:44 +0530

    code from class

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ import { loginschema } from "../components/index";
 //     height:"400px",
 //     backgroundImage:"url("+{background3}+")"
 // }
-import background3 from "../../public/images/background3.jpg"
+
 export const Login = () => {
     const initialValues = {
         email: "",
@@ -24,66 +24,56 @@ export const Login = () => {
             initialValues: initialValues,
             validationSchema: loginschema,
         })
-
     return (
-        // <div className=" container  ">
-        //     <img src ={background3} width={600} height={600} no-repeat/>
-        //   <div style={{backgroundImage:`url(${background3})`,
+        // <div className=" container-fluid image">
+        <div className="container-fluid image ">
+            <div className='wrapper '>
+                {/* <img src ={background3} /> */}
+                {/* <img src="{ require('./images/background3.jpg")} */}
+                <form onSubmit={handleSubmit} action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input
+                            type="text"
+                            id="Email"
+                            placeholder="Email"
+                            autoComplete="off"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
 
-        // }}> 
-        <div className="container background">
-            <div className="container-fluid ">
-                {/* <div className="image"> */}
-                {/* </div> */}
-                <div className='wrapper'>
+                        />
+                        {/* <FontAwesomeIcon icon={faUser} />  */}
+                    </div>
+                    {errors.email && touched.email ? (
+                        <p className="form-error">{errors.email}</p>
+                    ) : null}
+                    <div className="input-box">
 
-                    {/* <img src="{ require('./images/background3.jpg")} */}
-                    <form onSubmit={handleSubmit} action="">
-                        <h1>Login</h1>
-                        <div className="input-box">
-                            <input
-                                type="text"
-                                id="Email"
-                                placeholder="Email"
-                                autoComplete="off"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
+                        <input
+                            type="Password"
+                            placeholder="Password"
+                            id="Password"
+                            autoComplete="off"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </div>
+                    {errors.password && touched.password ? (
+                        <p className="form-error">{errors.password}</p>
+                    ) : null}
+                    {/* <FontAwesomeIcon icon={faLock} /> */}
 
-                            />
-                            {/* <FontAwesomeIcon icon={faUser} />  */}
-                        </div>
-                        {errors.email && touched.email ? (
-                            <p className="form-error">{errors.email}</p>
-                        ) : null}
-                        <div className="input-box">
-
-                            <input
-                                type="Password"
-                                placeholder="Password"
-                                id="Password"
-                                autoComplete="off"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                            />
-                        </div>
-                        {errors.password && touched.password ? (
-                            <p className="form-error">{errors.password}</p>
-                        ) : null}
-                        {/* <FontAwesomeIcon icon={faLock} /> */}
-
-                        <div className="remember-forgot py-4">
-                            <label><input type="checkbox" />Remember me</label>
-                            <NavLink to="/Forgotpassword">Forgot Password ?</NavLink>
-                        </div>
-                        <button type="submit">Login</button>
-                        <div className="signup-link">
-                            <p>Don't have a account ?<NavLink to="/Signup">signup</NavLink></p>
-                        </div>
-                    </form>
-                </div>
+                    <div className="remember-forgot py-4">
+                        <label><input type="checkbox" />Remember me</label>
+                        <NavLink to="/Forgotpassword">Forgot Password ?</NavLink>
+                    </div>
+                    <button type="submit">Login</button>
+                    <div className="signup-link">
+                        <p>Don't have a account ?<NavLink to="/Signup">signup</NavLink></p>
+                    </div>
+                </form>
             </div>
         </div>
-        //   </div >
-        //   </div >
+        // </div>
     )
 }
